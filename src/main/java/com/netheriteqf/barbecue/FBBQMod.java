330c007b70fbc5c3512cfed8e047905fcd60358d
MaoMao <98005009+AOMIAOMAO@users.noreply.github.com>
2024-02-16 20:59:01 +0800

    update

--- src/main/java/com/netheriteqf/barbecue/FBBQMod.java
+++ src/main/java/com/netheriteqf/barbecue/FBBQMod.java
@@ -1,18 +1,26 @@
 package com.netheriteqf.barbecue;
 
-import com.netheriteqf.barbecue.registry.FBBQBlock;
-import com.netheriteqf.barbecue.registry.FBBQBlockEntity;
-import com.netheriteqf.barbecue.registry.FBBQItem;
-
+import com.netheriteqf.barbecue.registry.FBBQBlocks;
+import com.netheriteqf.barbecue.registry.FBBQEntityTypes;
+import com.netheriteqf.barbecue.registry.FBBQItems;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FBBQMod implements ModInitializer {
-	public static String MODID = "fabric_barbecue";
+	public final static String MODID = "fabric_barbecue";
+	public static final Logger LOGGER = LoggerFactory.getLogger(FBBQMod.class);
 
 	@Override
 	public void onInitialize() {
-		FBBQItem.FBBQItem();
-		FBBQBlock.FBBQBlock();
-		FBBQBlockEntity.FBBQBlockEntity();
+		FBBQItems.registerModItems();
+		FBBQBlocks.registerModBlocks();
+		FBBQEntityTypes.registerModEntityTypes();
+		LOGGER.info("Hello World");
+	}
+
+	public static Identifier asID(String id){
+		return new Identifier(MODID, id);
 	}
 }
