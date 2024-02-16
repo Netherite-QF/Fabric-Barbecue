54f27ddca3ed65525929d6adaa4aaac782c5f2ed
MaoMao <98005009+AOMIAOMAO@users.noreply.github.com>
2024-02-16 20:59:37 +0800

    update

--- src/main/java/com/netheriteqf/barbecue/blockentities/GrillBlockEntity.java
+++ src/main/java/com/netheriteqf/barbecue/blockentities/GrillBlockEntity.java
@@ -1,15 +1,12 @@
 package com.netheriteqf.barbecue.blockentities;
 
-import com.netheriteqf.barbecue.registry.FBBQBlock;
-import com.netheriteqf.barbecue.registry.FBBQBlockEntity;
-
+import com.netheriteqf.barbecue.registry.FBBQEntityTypes;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
 
-public class GrillBlockEntity extends BlockEntity{
-
+public class GrillBlockEntity extends BlockEntity {
     public GrillBlockEntity(BlockPos pos, BlockState state) {
-        super(FBBQBlockEntity.GRILL_BLOCK_ENTITY, pos, state);
+        super(FBBQEntityTypes.GRILL_BLOCK_ENTITY, pos, state);
     }
 }
